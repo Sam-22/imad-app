31b7cceea91570cd9eb027b3563b001bb8efc961
Sam-22 <36440319+Sam-22@users.noreply.github.com>
2018-02-15 11:50:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //Change the image in index.html
 var element = document.getElementById('main-text'); 
-element.innerHTML = "<img src = 'http://0.media.dorkly.cvcdn.com/93/37/6af85e48981ebe35c3cda962730853b7.jpg'>"; 
+element.innerHTML = "New text"; 
 
 //Move the element
 var image = document.getElementById('image');
