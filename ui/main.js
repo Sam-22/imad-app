0ea81ef6591ad4ec0351a0d7f2632bde2a6503de
Sam-22 <36440319+Sam-22@users.noreply.github.com>
2018-02-15 12:27:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = "New text";
 //Move the element
 var img = document.getElementById('img');
 
+var marginleft = 0;
+function moveRight(){
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
+
 img.onclick = function(){
     var interval = setInterval(moveRight, 100);  
 };
