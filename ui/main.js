943fa073c6f354ead2bb0038131ac0c492b53758
Sam-22 <36440319+Sam-22@users.noreply.github.com>
2018-02-15 12:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-//Change the image in index.html
-var element = document.getElementById('main-text'); 
-element.innerHTML = "New text"; 
-
-//Move the element
-var img = document.getElementById('img');
-
-var marginleft = 0;
-function moveRight(){
-    marginleft = marginleft + 10;
-    img.style.marginleft = marginleft + 'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 100);  
+button.onclick = function(){
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
